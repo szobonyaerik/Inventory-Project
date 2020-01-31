7db8f0534e37c71707659d30b6e2dd67eac32129
Erik Szobonya <szobonyaerik@gmail.com>
2020-01-31 14:35:05 +0100

    see

---
+++ FinalInventory/FinalInventory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fuck");
+        }
+    }
+}
